61405552a319186975584c914b4e23bd67baf6ff
Łukasz Tucholski <42644138+lukasztucholski@users.noreply.github.com>
2018-11-01 16:19:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ function newNote() {
         noteID++;
         let date = actualDate();
         let newNoteFromConstructor = new NoteConstructor(noteTagsValue, noteContentValue, priority, noteID, date);
-        clearForm();
         sectionNotes.insertBefore(newNoteFromConstructor, sectionNotes.firstChild);
         allOfNotes.push(newNoteFromConstructor);
         allOfPriority.push(newNoteFromConstructor.dataset.priority);
         refreshStatistics();
+        clearForm();
     }
 }
 
